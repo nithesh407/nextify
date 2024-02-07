6a6bbb5922ba7b638d560dc63ee8d22b869507c0
Pradeep3238 <d.pradeep3238@gmail.com>
2024-02-07 22:56:10 +0530

    added profile component in homePage

---
+++ ui/components/HomeProfile/HomeProfile.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import { Avatar, Card } from "antd";
+import Meta from "antd/es/card/Meta";
+import {  GithubFilled, InstagramFilled, LinkedinFilled, TwitterOutlined, YoutubeFilled  } from "@ant-design/icons";
+
+import coverImg from '@/public/cover.jpg'
+
+type homeprofiletype={
+    profileName:string,
+    profileTag:string,
+    profileDescription:string,
+    profileImage:string,
+    linkedInURL:string,
+    githubURL:string,
+    twitterURL:string,
+    instagramURL:string
+}
+
+const HomeProfile:React.FC<homeprofiletype> = ({profileName,profileTag,profileDescription,profileImage,linkedInURL,githubURL,twitterURL,instagramURL}) =>{
+    return (
+        <Card
+            actions={[
+                <LinkedinFilled style={{color:'dark blue'}} src={linkedInURL}/>,
+                <GithubFilled style={{color:'dark grey'}} src={githubURL}/>,
+                <TwitterOutlined style={{color:'skyblue'}} src={twitterURL}/>,
+                <InstagramFilled style={{color:'purple'}} src={instagramURL}/>,
+            ]}
+            cover={
+                <Image height={120}  src={coverImg} alt={"coverImage"}></Image>
+            }
+        >
+            <Meta
+                style={{display:'flex',flexDirection:'column',marginTop:-60,alignItems:'center'}}
+                avatar={
+                    <Avatar size={90} src={profileImage}/>
+                }
+                title={profileName}
+                description={profileTag}
+            >
+            </Meta>
+                <p style={{marginTop:20,textAlign:'center'}}>{profileDescription}</p>
+        </Card>
+    )
+}
+export default HomeProfile;
