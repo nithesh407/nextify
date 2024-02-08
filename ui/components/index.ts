0c5c591cc76a635aa6184c717455b12447b0a05b
nithesh407 <nitheshravikumar13631@gmail.com>
2024-02-08 11:07:29 +0530

    added floatbutton and calender component

--- ui/components/index.ts
+++ ui/components/index.ts
@@ -2,10 +2,13 @@ import Navbar from "./Navbar/Navbar";
 import Post from "./Post/post";
 import CreatePost from "./CreatePost/CreatePost";
 import HomeProfile from "./HomeProfile/HomeProfile";
-
+import HomeCalender from "./HomeCalender/HomeCalender";
+import FloatButtonComponent from "./FloatButtonComponent/FloatButtonComponent";
 export {
     Navbar,
     HomeProfile,
     Post,
-    CreatePost
+    CreatePost,
+    HomeCalender,
+    FloatButtonComponent
 }
